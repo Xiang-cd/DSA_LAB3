c57e34de7bcf34d532e43d27aa27a780d19b45c8
Mac-2读者 <iMac-2@MAC2deiMac.local>
2021-11-18 21:33:43 +0800

    add files

---
+++ BST.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Mac-2读者 on 2021/11/18.
+//
+
+#ifndef LAB3_BST_HPP
+#define LAB3_BST_HPP
+
+#endif //LAB3_BST_HPP
---
+++ BinTree.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Mac-2读者 on 2021/11/18.
+//
+
+#ifndef LAB3_BINTREE_HPP
+#define LAB3_BINTREE_HPP
+
+#endif //LAB3_BINTREE_HPP
